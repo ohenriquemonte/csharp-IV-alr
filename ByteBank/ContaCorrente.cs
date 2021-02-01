5dcaa9760191e67172444e3732fce70ba754fcc6
ohenriquemonte <montenegrohenrique@gmail.com>
2021-02-01 20:41:03 -0300

    03 - ArgumentException, ParamName e nameof

--- ByteBank/ContaCorrente.cs
+++ ByteBank/ContaCorrente.cs
@@ -1,89 +1,84 @@
 ﻿// using _05_ByteBank;
 
+using System;
+
 namespace ByteBank
 {
-    public class ContaCorrente
-    {
-        public Cliente Titular { get; set; }
+	public class ContaCorrente
+	{
+		public Cliente Titular { get; set; }
 
-        public static int TotalDeContasCriadas { get; private set; }
+		public static double TaxaOperacao { get; private set; }
+		public static int TotalDeContasCriadas { get; private set; }
 
-
-        private int _agencia;
-        public int Agencia
-        {
-            get
-            {
-                return _agencia;
-            }
-            set
-            {
-                if (value <= 0)
-                {
-                    return;
-                }
-
-                _agencia = value;
-            }
-        }
-        public int Numero { get; set; }
-
-        private double _saldo = 100;
-
-        public double Saldo
-        {
-            get
-            {
-                return _saldo;
-            }
-            set
-            {
-                if (value < 0)
-                {
-                    return;
-                }
-
-                _saldo = value;
-            }
-        }
+		public int Agencia { get; }
+		public int Numero { get; }
 
 
-        public ContaCorrente(int agencia, int numero)
-        {
-            Agencia = agencia;
-            Numero = numero;
+		private double _saldo = 100;
 
-            TotalDeContasCriadas++;
-        }
+		public double Saldo
+		{
+			get
+			{
+				return _saldo;
+			}
+			set
+			{
+				if (value < 0)
+				{
+					return;
+				}
 
-
-        public bool Sacar(double valor)
-        {
-            if (_saldo < valor)
-            {
-                return false;
-            }
-
-            _saldo -= valor;
-            return true;
-        }
-
-        public void Depositar(double valor)
-        {
-            _saldo += valor;
-        }
+				_saldo = value;
+			}
+		}
 
 
-        public bool Transferir(double valor, ContaCorrente contaDestino)
-        {
-            if (_saldo < valor)
-            {
-                return false;
-            }
+		public ContaCorrente(int agencia, int numero)
+		{
+			if (numero <= 0)
+				throw new ArgumentException("Número da Conta deve ser maior que 0!", nameof(numero));
 
-            _saldo -= valor;
-            contaDestino.Depositar(valor);
-            return true;
-        }
-    }
+			if (agencia <= 0)
+				throw new ArgumentException("Agencia deve ser maior que 0!", nameof(agencia));
+
+			Agencia = agencia;
+			Numero = numero;
+
+			TaxaOperacao = 30 / TotalDeContasCriadas;
+
+			TotalDeContasCriadas++;
+		}
+
+
+		public bool Sacar(double valor)
+		{
+			if (_saldo < valor)
+			{
+				return false;
+			}
+
+			_saldo -= valor;
+			return true;
+		}
+
+		public void Depositar(double valor)
+		{
+			_saldo += valor;
+		}
+
+
+		public bool Transferir(double valor, ContaCorrente contaDestino)
+		{
+			if (_saldo < valor)
+			{
+				return false;
+			}
+
+			_saldo -= valor;
+			contaDestino.Depositar(valor);
+			return true;
+		}
+	}
 }
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace ByteBank
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.ReadLine();
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				ContaCorrente conta = new ContaCorrente(0, 1);
+				Console.WriteLine(ContaCorrente.TaxaOperacao);
+			}
+			catch (ArgumentException ex)
+			{
+				Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException!");
+				Console.WriteLine(ex.Message);
+				Console.WriteLine("Argumento com problema: {0}", ex.ParamName);
+				//Console.WriteLine(ex.StackTrace);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				Console.WriteLine(ex.StackTrace);
+			}
+
+			Console.ReadLine();
+		}
+	}
 }
